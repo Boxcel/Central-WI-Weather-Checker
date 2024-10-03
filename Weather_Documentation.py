94dbf079e133a447e6c0b48460169131db479b76
Boxcel <Boxcel@proton.me>
2024-10-03 11:30:31 -0500

    Update Weather_Documentation.py

--- Weather_Documentation.py
+++ Weather_Documentation.py
@@ -6,6 +6,6 @@ import dataclasses
 Weather = ['Clear',"Cloudy", "Overcast", "Foggy", "Rain", "Snow"]
 
 Complex_Weather = ['Wind Chill', 'Ice', 'Sleet', 'Freezing Rain', 'Freezing Drizzle', 'Freezing Fog', 'Blowing Snow', 'Hail',]
-Variables =['Light', 'Moderate', 'Heavy', 'Severe']
+Variables = ['Light', 'Moderate', 'Heavy', 'Severe']
 Complex_Variables = ['Patchy', 'Intermittent', 'Continuous', 'Occasional', 'Periods of', 'Showers', 'Drizzle', 'Flurries', 'Squalls', 'Thundershowers']
 Extreme_Weather = ['Tornado', 'Hurricane', 'Blizzard', 'Flood', 'Drought', 'Heatwave', 'Coldwave', 'Thunderstorm', 'Hailstorm', 'Tsunami', "earthquake"]
