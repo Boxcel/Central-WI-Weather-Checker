0e7818f29dcf29a333c688a88650b332bbefca1b
Boxcel <Boxcel@proton.me>
2024-10-03 10:35:38 -0500

    Update ACS01.py
    Lists added for weather variables.

--- ACS01.py
+++ ACS01.py
@@ -1,5 +1,11 @@
-print("Welcome to Breeze!")
-Admin = 1
-Visitor = 2
-input("Welcome to the AcousticSystems app,")
-print("")
+import random
+import datetime
+import time
+import dataclasses
+
+Weather = ['Clear',"Cloudy", "Overcast", "Foggy", "Rain", "Snow"]
+
+Complex_Weather = ['Wind Chill', 'Ice', 'Sleet', 'Freezing Rain', 'Freezing Drizzle', 'Freezing Fog', 'Blowing Snow', 'Hail',]
+Variables =['Light', 'Moderate', 'Heavy', 'Severe']
+Complex_Variables = ['Patchy', 'Intermittent', 'Continuous', 'Occasional', 'Periods of', 'Showers', 'Drizzle', 'Flurries', 'Squalls', 'Thundershowers']
+Extreme_Weather = ['Tornado', 'Hurricane', 'Blizzard', 'Flood', 'Drought', 'Heatwave', 'Coldwave', 'Thunderstorm', 'Hailstorm', 'Tsunami', "earthquake"]
