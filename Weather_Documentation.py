91abe22ecb28f57de061263636ea5ee14ff143f9
Boxcel <Boxcel@proton.me>
2024-10-04 08:06:54 -0500

    Update Weather_Documentation.py

--- Weather_Documentation.py
+++ Weather_Documentation.py
@@ -3,9 +3,22 @@ import datetime
 import time
 import dataclasses
 
-Weather = ['Clear',"Cloudy", "Overcast", "Foggy", "Rain", "Snow"]
+Weather = ['Clear', 'Cloudy', 'Overcast', 'Foggy', 'Rain', 'Snow']
+Weather == input(print("What is your current weather?: "))
+if Weather == 'Clear':  
+ print("Your current weather is Clear")
+elif Weather == 'Cloudy':
+  print("Your current weather is Cloudy")
+elif Weather == 'Overcast':
+    print("Your current weather is Overcast")
+elif Weather == 'Foggy':
+   print("Your current weather is Foggy")
+elif Weather == 'Rain':
+   print("Your current weather is Rainy")
+elif Weather == 'Snow':
+    print("Your current weather is Snowy")
+#Complex_Weather = ['Wind Chill', 'Ice', 'Sleet', 'Freezing Rain', 'Freezing Drizzle', 'Freezing Fog', 'Blowing Snow', 'Hail',]
 
-Complex_Weather = ['Wind Chill', 'Ice', 'Sleet', 'Freezing Rain', 'Freezing Drizzle', 'Freezing Fog', 'Blowing Snow', 'Hail',]
-Variables = ['Light', 'Moderate', 'Heavy', 'Severe']
-Complex_Variables = ['Patchy', 'Intermittent', 'Continuous', 'Occasional', 'Periods of', 'Showers', 'Drizzle', 'Flurries', 'Squalls', 'Thundershowers']
-Extreme_Weather = ['Tornado', 'Hurricane', 'Blizzard', 'Flood', 'Drought', 'Heatwave', 'Coldwave', 'Thunderstorm', 'Hailstorm', 'Tsunami', "earthquake"]
+#Variables =['Light', 'Moderate', 'Heavy', 'Severe']
+#Complex_Variables = ['Patchy', 'Intermittent', 'Continuous', 'Occasional', 'Periods of', 'Showers', 'Drizzle', 'Flurries', 'Squalls', 'Thundershowers']
+#Extreme_Weather = ['Tornado', 'Hurricane', 'Blizzard', 'Flood', 'Drought', 'Heatwave', 'Coldwave', 'Thunderstorm', 'Hailstorm', 'Tsunami', "earthquake"]
